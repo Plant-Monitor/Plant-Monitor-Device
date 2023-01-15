cb2498aaba783d3364998eb48d142c543576306e
Guy-Jacques Isombe <isombeg@mcmaster.ca>
2023-01-15 01:02:53 -0500

    init commit

---
+++ src/utils/singleton.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Singleton struct{}
+
+var lock = &sync.Mutex{}
+
+func getInstance[T any](
+	instance *T,
+	constructor func(interface{}) *T,
+	initParams interface{},
+) T {
+	if instance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if instance == nil {
+			fmt.Println("Creating Singleton instance now.")
+			instance = constructor(initParams)
+		} else {
+			fmt.Println("Singleton instance already created.")
+		}
+	} else {
+		fmt.Println("Singleton instance already created.")
+	}
+
+	return *instance
+}
